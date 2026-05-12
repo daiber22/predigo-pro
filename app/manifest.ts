ece36540a1dc255883403ad0cab91cd2e49973d9
daiber22 <rivasdaiber@gmail.com>
2026-05-11 22:50:33 -0500

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -28,7 +28,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icons/icon-512-maskable.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src: '/icons/apple-touch-icon.png',
