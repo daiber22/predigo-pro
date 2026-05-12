d64c149c8265c530e5582d91e332565c7694af43
daiber22 <rivasdaiber@gmail.com>
2026-05-11 23:49:37 -0500

    Update manifest.ts

--- manifest.ts
+++ manifest.ts
@@ -13,30 +13,27 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0b1220',
     lang: 'es-CO',
     categories: ['sports', 'productivity', 'utilities'],
-    icons: [
-      {
-        src: '/icons/icon-192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/icons/icon-512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-      {
-       {
-  src: '/icons/icon-512-maskable.png',
-  sizes: '512x512',
-  type: 'image/png',
-  purpose: 'maskable',
-},
-{
-      {
-        src: '/icons/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-      },
-    ],
-  };
+   icons: [
+  {
+    src: '/icons/icon-192.png',
+    sizes: '192x192',
+    type: 'image/png',
+  },
+  {
+    src: '/icons/icon-512.png',
+    sizes: '512x512',
+    type: 'image/png',
+  },
+  {
+    src: '/icons/icon-512-maskable.png',
+    sizes: '512x512',
+    type: 'image/png',
+    purpose: 'maskable',
+  },
+  {
+    src: '/icons/apple-touch-icon.png',
+    sizes: '180x180',
+    type: 'image/png',
+  },
+],
 }
